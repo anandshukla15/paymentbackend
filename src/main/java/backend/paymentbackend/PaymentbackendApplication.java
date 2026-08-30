6d8285fd4ba1228802fa9a56dfd680c01c077c4d
anandshukla15 <anandshukla1742@gmail.com>
2026-08-31 01:40:22 +0530

    Initial commit

---
+++ src/main/java/backend/paymentbackend/PaymentbackendApplication.java
@@ -0,0 +1,13 @@
+package backend.paymentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/paymentbackend/PaymentbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.paymentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
