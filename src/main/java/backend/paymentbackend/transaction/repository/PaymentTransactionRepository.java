ba61d2416de62323cb2ad29514d70e0fea081120
anandshukla15 <anandshukla1742@gmail.com>
2026-09-01 02:06:53 +0530

    repository  done

--- src/main/java/backend/paymentbackend/transaction/repository/PaymentTransactionRepository.java
+++ src/main/java/backend/paymentbackend/transaction/repository/PaymentTransactionRepository.java
@@ -1,4 +1,22 @@
 package backend.paymentbackend.transaction.repository;
 
-public class PaymentTransactionRepository {
+import backend.paymentbackend.transaction.entity.PaymentTransaction;
+import backend.paymentbackend.transaction.entity.TransactionStatus;
+import backend.paymentbackend.transaction.entity.TransactionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PaymentTransactionRepository
+        extends JpaRepository<PaymentTransaction, UUID> {
+
+    List<PaymentTransaction> findByPaymentId(UUID paymentId);
+
+    List<PaymentTransaction> findByPaymentIdAndType(
+            UUID paymentId,
+            TransactionType type
+    );
+
+    List<PaymentTransaction> findByStatus(TransactionStatus status);
 }
